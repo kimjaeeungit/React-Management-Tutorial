609c30610d4ef7d1752f6b493803573f73e65324
kimjaeeungit <dmjfhe12@gmail.com>
2021-12-21 03:11:51 +0900

    Apply Material UI

--- management/src/App.js
+++ management/src/App.js
@@ -2,6 +2,24 @@ import './App.css';
 
 import React, { Component } from 'react';
 import Customer from './components/Customer';
+import { Paper } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme =>({
+  root : {
+    width : '100%',
+    margintTop : theme.spacing.unit *3,
+    overFlowX : "auto"
+  },
+  table : {
+    minWidth : 1000
+  }
+})
 
 const customers = [
 {
@@ -32,13 +50,27 @@ const customers = [
 class App extends Component {
   //Component란 웹문서에서 어떠한 내용을 보여주기 위한 기본적인 단위
   render() {
+    //classes 변수를 만들어 위에서 정의한 styles가 적용되도록 하기
+    const{ classes } = this.props;
     return (
       //map을 사용할때는 key라는 이름의 props를 사용해야한다.
-      <div>
-        {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );}) }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+          {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );}) }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- management/src/components/Customer.js
+++ management/src/components/Customer.js
@@ -1,4 +1,6 @@
 import React from "react"; 
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component{
     //render()는 항상 수행되는 내용으로써 Customer라는 컴포넌트를 실제 화면에 그리고자 할때 수행
@@ -6,37 +8,15 @@ class Customer extends React.Component{
         return(
             //props는 기본적으로 react component가 포함하고있는 내용이라 this키워드를 이용하기
             //jsx문법은 두개이상의 요소가 있을시 반드시 div같은 태그로 감싸야 한다.
-            <div>
-               <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
-               <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         ); 
-    }
-}
-
-//이미지 이름 아이디 값 출력
-class CustomerProfile extends React.Component{
-    render(){
-        return(
-            <div>
-                <img src={this.props.image} alt="profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        );
-
-    }
-}
-
-//남은 데이터 출력
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        );
     }
 }
 
