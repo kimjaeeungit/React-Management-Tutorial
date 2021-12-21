75195f052201c1c4c138e8813653c63b414cc8d6
kimjaeeungit <dmjfhe12@gmail.com>
2021-12-22 03:12:50 +0900

    Add Get Customers REST API Module

--- management/client/src/App.js
+++ management/client/src/App.js
@@ -21,33 +21,28 @@ const styles = theme =>({
   }
 })
 
-const customers = [
-{
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '김재은',
-  'birthday' : '970220',
-  'gender' : '여자',
-  'job' : '취준생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '김재영',
-  'birthday' : '980901',
-  'gender' : '여자',
-  'job' : '직장인'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '감영호',
-  'birthday' : '630912',
-  'gender' : '남자',
-  'job' : '아빠'
-}
-]
+
 class App extends Component {
+  //고객데이터는 처음에 비어있다가 서버로부터 데이터를 받으면 그때 데이터가 재구성
+  //데이터가 변경될 수 있어서 state로 customer변수를 명시
+  state = {
+    customers : ""
+  }
+  //api서버에 접근해서 데이터를 받아오는 작업
+  //모든 컴포넌트가 mount가 되었을때 실행되는 부분(준비가 완료된 상태)
+  componentDidMount(){
+    //담긴 데이터가 body인데 callApi()가 불러와져서 
+    this.callApi() 
+      .then(res => this.setState({customers : res})) //then함수로 하여금 res로 변수의 이름이 바뀌고 customers라는 state의 변수에 값을 넣어줌
+      .catch(err => console.log(err)); //오류 발생하면 오류 출력
+  }
+  //callApi는 비동기적으로 내용 수행
+  callApi = async () =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   //Component란 웹문서에서 어떠한 내용을 보여주기 위한 기본적인 단위
   render() {
     //classes 변수를 만들어 위에서 정의한 styles가 적용되도록 하기
@@ -65,7 +60,8 @@ class App extends Component {
             <TableCell>직업</TableCell>
           </TableHead>
           <TableBody>
-          {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );}) }
+          {this.state.customers ? this.state.customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> );
+          }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- management/server.js
+++ management/server.js
@@ -6,8 +6,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true}));
 
-app.get('/api/hello', (req,res) =>{
-    res.send({message : 'Hello Express!'})
+//client가 /api/customers에 접속을 하게되면 client에게 데이터를 반환할 수 있도록 하기
+///api/customers에 접속하면  json 형태로 데이터 반환
+app.get('/api/customers',(req,res) =>{
+    res.send([
+        {
+          'id' : 1,
+          'image' : 'https://placeimg.com/64/64/1',
+          'name' : '김재은',
+          'birthday' : '970220',
+          'gender' : '여자',
+          'job' : '취준생'
+        },
+        {
+          'id' : 2,
+          'image' : 'https://placeimg.com/64/64/2',
+          'name' : '김재영',
+          'birthday' : '980901',
+          'gender' : '여자',
+          'job' : '직장인'
+        },
+        {
+          'id' : 3,
+          'image' : 'https://placeimg.com/64/64/3',
+          'name' : '감영호',
+          'birthday' : '630912',
+          'gender' : '남자',
+          'job' : '아빠'
+        }
+        ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
